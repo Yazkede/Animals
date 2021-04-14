c9bc739ba96f56469750a2f35dd1275ae4132910
Yazkede <missclick79@gmail.com>
2021-04-15 01:57:32 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+   $('.header__burger').click(function () {
+      $('.header__burger, .header__list').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+
+   $('.header__menu').click(function () {
+      $('.header__burger, .header__list').removeClass('active');
+      $('body').removeClass('lock');
+   });
+
+   $('.slider').slick({
+      arrows: true,
+      dots: true,
+      slidesToShow: 1,
+      autoplay: true,
+      speed: 500,
+      autoplaySpeed: 1500,
+      responsive: [
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 2
+            }
+         },
+         {
+            breakpoint: 550,
+            settings: {
+               slidesToShow: 1
+            }
+         }
+      ]
+   }
+   );
+});
+
